527ca18e7fbd8d398964ca210f269e884fc34a0b
3s777 <ivansinica@gmail.com>
2017-07-11 14:34:31 +0300

    Fix eslint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'normalize.css';
 import './App.css';
@@ -7,19 +7,12 @@ import Complexes from './Complexes/List';
 import Complex from './Complexes/Show';
 import Footer from './Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Header />
-          <Route exact path="/" component={Complexes} />
-          <Route path="/complex" component={Complex} />
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
-}
-
-export default App;
+export default () =>
+  (<Router>
+    <div>
+      <Header />
+      <Route exact path="/" component={Complexes} />
+      <Route path="/complex" component={Complex} />
+      <Footer />
+    </div>
+  </Router>);
